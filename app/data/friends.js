8cd21f011f35c9d9fdcafab78921b9b6945b2221
Brian Nickila <brian.nickila@gmail.com>
2018-12-22 21:00:46 -0600

    Made some small adjustments.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,68 +1,21 @@
 var friends =
     [
         {
-            name: "Garfield",
-            img: "https://vignette.wikia.nocookie.net/garfield/images/9/9f/GarfieldCharacter.jpg/revision/latest/scale-to-width-down/321?cb=20180421131132",
-            score: [
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1,
-                1
-            ]
-        },
-        {
-            name: "Orko",
-            img: "https://i.ebayimg.com/images/g/xJcAAOSwDNdV2glw/s-l300.jpg",
-            score: [
-                2,
-                2,
-                2,
-                2,
-                2,
-                2,
-                2,
-                2,
-                2,
-                2
-            ]
-        },
-        {
             name: "Marjory",
             img: "https://www.syfy.com/sites/syfy/files/styles/1200x680/public/wire/legacy/tumblr_static_tumblr_static_filename_640.jpg?itok=xGXErNZC",
             score: [
+                2,
+                5,
+                5,
+                1,
+                2,
                 3,
-                3,
-                3,
-                3,
-                3,
-                3,
-                3,
-                3,
-                3,
-                3
+                4,
+                5,
+                1,
+                2
             ]
-        },
-        {
-            name: "Alf",
-            img: "https://ih0.redbubble.net/image.422284817.3738/flat,550x550,075,f.u1.jpg",
-            score: [
-                4,
-                4,
-                4,
-                4,
-                4,
-                4,
-                4,
-                4,
-                4,
-                4
-            ]
+        
         }
     ];
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,28 +2,18 @@ var friends = require("../data/friends.js");
 var match = {};
 module.exports = function (app, path) {
 
-
-
-
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-
-
-
-
-
     app.post("/api/friends", function (req, res) {
-        // all the logic should be here, not in survey
-        // when html makes post request, $.post request in html, saves info as req.body 
         var newFriend = req.body;
         function add(a, b) {
             return a + b;
         }
-        // A. Push all scores from newFriend into an array.
-        // B. Find difference of all scores from newFriend array together and save sum as a variable.
         var newScoreArr = [];
+        
         for (i = 0; i < newFriend.score.length; i++) {
+            parseInt(newFriend.score[i]);
             newScoreArr.push(newFriend.score[i]);
         }
         var diff = [];
@@ -33,6 +23,7 @@ module.exports = function (app, path) {
             var friendsScoreArr = [];
         
             for (i = 0; i < friends[j].score.length; i++) {
+                parseInt(friends[j].score[i]);
                 friendsScoreArr.push(friends[j].score[i])
             }
         
@@ -61,59 +52,7 @@ module.exports = function (app, path) {
         }
         indexOfSmallest(sumArr);
         friends.push(newFriend);
+        console.log("ending " + newFriend.score[0])
         res.json(match);
     });
-
-
 };
-
-
-
-
-
-
-
-// var express = require("express");
-// var path = require("path");
-
-// var app = express();
-// var PORT = 3000;
-
-// app.use(express.urlencoded({
-//     extended: true
-// }));
-// app.use(express.json());
-
-
-// app.get("/api/friends", function (req, res) {
-//     return res.json(friends);
-// });
-
-// app.get("/api/friends/:friend", function (req, res) {
-//     var chosen = req.params.friend;
-
-//     console.log(chosen);
-
-//     for (var i = 0; i < friends.length; i++) {
-//         if (chosen === friends[i].routeName) {
-//             return res.json(friends[i]);
-//         }
-//     }
-//     return res.json(false);
-// });
-
-// app.post("/api/friends", function (req, res) {
-
-//     var newFriend = req.body;
-
-//     newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-
-//     console.log(newFriend);
-
-//     friends.push(newFriend);
-
-//     res.json(newFriend);
-// });
-// app.listen(PORT, function () {
-//     console.log("App listening on PORT " + PORT);
-// });
